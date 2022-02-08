b9629a26a2c2984e09762f5680f38f8050daaf67
sudo-homebrew <70418121+sudo-homebrew@users.noreply.github.com>
2022-02-08 14:40:14 +0900

    Add files via upload

---
+++ APIs/README.md
@@ -0,0 +1,30 @@
+# 패키지
+## json 
+
+## import_ipynb
+ipynb 파일을 import하기 위해 사용
+py 파일로 변환할 경우 필요 없음
+
+## requests
+
+## playsound
+
+## scipy.io.wavfile
+
+
+## picamera 
+ 사진 촬영
+
+# 사용법
+```python
+import from APIs.get_emotion_by_text import get_emotion_by_text
+import from APIs.get_emotion_by_image import get_emotion_by_image
+
+result1 = get_emotion_by_text('안녕하세요')
+result2 = get_emotion_by_image() #image file must be located in /APIs.
+print(result1)
+print(result2)
+```
+12/08 
+.py로 변환한 모듈들을 py_modules 폴더에 올려두었습니다.
+
---
+++ APIs/py_modules/take_new_image.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import cv2
+import time
+start = time.time()  # 시작 시간 저장
+
+
+
+# In[6]:
+
+def take_new_image():
+	start = time.time()  # 시작 시간 저장
+	cap = cv2.VideoCapture(0) # 노트북 웹캠을 카메라로 사용
+	cap.set(3,1280) # 너비
+	cap.set(4,720) # 높이
+
+	ret, frame = cap.read() # 사진 촬영
+
+	cv2.imwrite('self camera test.jpg', frame) # 사진 저장
+
+	cap.release()
+	cv2.destroyAllWindows()
+	print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
+
+
+
+# In[ ]:
+
+'''
+
+
